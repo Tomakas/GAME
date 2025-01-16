1ae9ec92f4f7cc3b316be30b6faf4d9b9920c2b1
Tomas <kvasnica.tomas@seznam.cz>
2025-01-16 21:02:06 +0100

    Init

--- lib/screens/game_screen.dart
+++ lib/screens/game_screen.dart
@@ -5,30 +5,22 @@ class GameScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // Vytvoříme seznam 10 položek, přičemž každá položka bude
-    // ExpansionTile s 3 pododkazy
-    final List<Widget> expansionTiles = List.generate(10, (index) {
-      return ExpansionTile(
-        title: Text('Odkaz ${index + 1}'),
-        children: List.generate(6, (subIndex) {
-          return ListTile(
-            title: Text('Pododkaz ${subIndex + 1}'),
-            onTap: () {
-              // Zatím bez akce
-            },
-          );
-        }),
-      );
-    });
-
     return Scaffold(
-      // Můžeme vrátit Scaffold, pokud chceme pro GameScreen vlastní AppBar,
-      // anebo jen prosté widgety. Pro ukázku použijeme Scaffold.
       appBar: AppBar(
         title: const Text('Game'),
       ),
-      body: ListView(
-        children: expansionTiles,
+      body: Center(
+        child: GestureDetector(
+          onTap: () {
+            // Zatím bez specifické akce
+          },
+          child: Image.asset(
+            'lib/res/levels.jpg',
+            fit: BoxFit.cover, // Obrázek bude roztažen na celou obrazovku
+            width: double.infinity,
+            height: double.infinity,
+          ),
+        ),
       ),
     );
   }
